8176623c090a903de8eeb8a7e7db3ddec099d571
Chong <chong.guo@vidrovr.com>
2018-04-11 14:37:19 -0400

    [Add] map view apis

---
+++ src/Mapview.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export class Container extends React.Component {
+  render() {
+    if (!this.props.loaded) {
+      return <div>Loading...</div>
+    }
+    return (
+      <div>Map will go here</div>
+    )
+  }
+}
+
+export default GoogleApiComponent({
+  apiKey: __GAPI_KEY__
+})(Container)
